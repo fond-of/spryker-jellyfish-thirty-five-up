65f98877565c6f52a5297dae2b78bb7df9003903
Julian Zimmermann <julian.zimmermann@fondof.de>
2020-11-25 11:08:06 +0100

    - added tests

---
+++ tests/FondOfSpryker/Zed/JellyfishThirtyFiveUp/Dependency/Facade/JellyfishThirtyFiveUpToThirtyFiveUpFacadeBridgeTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace FondOfSpryker\Zed\JellyfishThirtyFiveUp\Dependency\Facade;
+
+use Codeception\Test\Unit;
+use FondOfSpryker\Zed\ThirtyFiveUp\Business\ThirtyFiveUpFacade;
+use Generated\Shared\Transfer\ThirtyFiveUpOrderTransfer;
+use Orm\Zed\ThirtyFiveUp\Persistence\ThirtyFiveUpOrder;
+
+class JellyfishThirtyFiveUpToThirtyFiveUpFacadeBridgeTest extends Unit
+{
+    /**
+     * @var \FondOfSpryker\Zed\JellyfishThirtyFiveUp\Dependency\Facade\JellyfishThirtyFiveUpToThirtyFiveUpFacadeBridge
+     */
+    protected $bridge;
+
+    /**
+     * @var \PHPUnit\Framework\MockObject\MockObject|\FondOfSpryker\Zed\ThirtyFiveUp\Business\ThirtyFiveUpFacadeInterface
+     */
+    protected $facadeMock;
+
+    /**
+     * @var \Orm\Zed\ThirtyFiveUp\Persistence\ThirtyFiveUpOrder|\PHPUnit\Framework\MockObject\MockObject
+     */
+    protected $thirtyFiveUpOrderMock;
+
+    /**
+     * @var \Generated\Shared\Transfer\ThirtyFiveUpOrderTransfer|\PHPUnit\Framework\MockObject\MockObject
+     */
+    protected $orderTransferMock;
+
+    /**
+     * @return void
+     */
+    protected function _before(): void
+    {
+        $this->facadeMock = $this->getMockBuilder(ThirtyFiveUpFacade::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->thirtyFiveUpOrderMock = $this->getMockBuilder(ThirtyFiveUpOrder::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->orderTransferMock = $this->getMockBuilder(ThirtyFiveUpOrderTransfer::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->bridge = new JellyfishThirtyFiveUpToThirtyFiveUpFacadeBridge(
+            $this->facadeMock
+        );
+    }
+
+    /**
+     * @return void
+     */
+    public function testConvertThirtyFiveUpOrderEntityToTransfer(): void
+    {
+        $this->facadeMock->expects($this->once())
+            ->method('convertThirtyFiveUpOrderEntityToTransfer')
+            ->willReturn($this->orderTransferMock);
+
+        $this->bridge->convertThirtyFiveUpOrderEntityToTransfer($this->thirtyFiveUpOrderMock);
+    }
+}
